444e42c0a63cfc4afdcd3e5372a346bb6f254cc8
Cristian <crisalvfer14@gmail.com>
2026-09-19 22:31:58 +0200

    feat: soluciones secuencial, async y mejor rendimiento

--- DesayunoAsincrono/Services/DesayunoService.cs
+++ DesayunoAsincrono/Services/DesayunoService.cs
@@ -1,10 +1,9 @@
+using DesayunoAsincrono.Models;
+
+namespace DesayunoAsincrono.Services;
+
 public class DesayunoService : IDesayunoService
 {
-    public void PrepararSecuencial(IEnumerable<Accion> acciones) { }
-    public Task PrepararConAsyncAwaitAsync(IEnumerable<Accion> acciones) { }
-    public Task PrepararMejorRendimientoAsync(IEnumerable<Accion> acciones) { }
-    public Task<bool> PrepararConTimeoutAsync(Func<Task> solucion, int timeoutMs) { }
-    
     public void PrepararSecuencial(IEnumerable<Accion> acciones)
     {
         foreach (var accion in acciones)
@@ -15,11 +14,51 @@ public class DesayunoService : IDesayunoService
         }
     }
 
-    public Task PrepararConAsyncAwaitAsync(IEnumerable<Accion> acciones)
-        => throw new NotImplementedException();
-    public Task PrepararMejorRendimientoAsync(IEnumerable<Accion> acciones)
-        => throw new NotImplementedException();
+    public async Task PrepararAsincronoAsync(IEnumerable<Accion> acciones)
+    {
+        foreach (var accion in acciones)
+        {
+            await EjecutarAccionAsync(accion);
+        }
+    }
+
+    public async Task PrepararMejorRendimientoAsync(IEnumerable<Accion> acciones)
+    {
+        var lista = acciones.ToList();
+        var cafe = lista.First(a => a.Nombre == "Café");
+        var sarten = lista.First(a => a.Nombre == "Sartén");
+        var huevos = lista.First(a => a.Nombre == "Huevos");
+        var bacon = lista.First(a => a.Nombre == "Bacon");
+        var pan = lista.First(a => a.Nombre == "Pan");
+        var mantequilla = lista.First(a => a.Nombre == "Mantequilla");
+        var zumo = lista.First(a => a.Nombre == "Zumo");
+
+        var tareaCafe = EjecutarAccionAsync(cafe);
+        var tareaZumo = EjecutarAccionAsync(zumo);
+
+        var tareaSarten = EjecutarAccionAsync(sarten);
+        var tareaHuevos = EjecutarDespuesDe(tareaSarten, huevos);
+        var tareaBacon = EjecutarDespuesDe(tareaSarten, bacon);
+
+        var tareaPan = EjecutarAccionAsync(pan);
+        var tareaMantequilla = EjecutarDespuesDe(tareaPan, mantequilla);
+
+        await Task.WhenAll(tareaCafe, tareaZumo, tareaSarten, tareaHuevos, tareaBacon, tareaPan, tareaMantequilla);
+    }
+
     public Task<bool> PrepararConTimeoutAsync(Func<Task> solucion, int timeoutMs)
         => throw new NotImplementedException();
-    
+
+    private static async Task EjecutarAccionAsync(Accion accion)
+    {
+        Console.WriteLine($"Iniciando: {accion.Nombre}");
+        await Task.Delay(accion.DuracionMs);
+        Console.WriteLine($"Terminado: {accion.Nombre}");
+    }
+
+    private static async Task EjecutarDespuesDe(Task dependencia, Accion accion)
+    {
+        await dependencia;
+        await EjecutarAccionAsync(accion);
+    }
 }
--- DesayunoAsincrono/Services/IDesayunoService.cs
+++ DesayunoAsincrono/Services/IDesayunoService.cs
@@ -1,11 +1,12 @@
+using DesayunoAsincrono.Models;
 
-namespace DesayunoAsincrono.Service;
+namespace DesayunoAsincrono.Services;
 
 public interface IDesayunoService
 {
     void PrepararSecuencial(IEnumerable<Accion> acciones);
-    Task prepararAsincronoAsync(IEnumerable<Accione> acciones);
-    Task PrepararMejorRendimiento(IEnumerable<Accion> acciones);
-    Task<bool> PrepararConMejorTiempo(Func<Task> solucion, int timeMs);
+    Task PrepararAsincronoAsync(IEnumerable<Accion> acciones);
+    Task PrepararMejorRendimientoAsync(IEnumerable<Accion> acciones);
+    Task<bool> PrepararConTimeoutAsync(Func<Task> solucion, int timeMs);
 }
 
