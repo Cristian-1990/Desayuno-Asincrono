cc48bcea11fc2aa28287e3aa701a2a449b0a0e61
Cristian <crisalvfer14@gmail.com>
2026-09-20 00:27:35 +0200

    chore: proyecto de tests NUnit

---
+++ DesayunoAsincrono.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿namespace DesayunoAsincrono.Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
